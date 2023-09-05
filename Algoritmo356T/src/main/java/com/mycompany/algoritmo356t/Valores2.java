0b6110d19f4fa3c9b515c16c5e6c241f7c1588f1
Dgandalecio <127789911+Dgandalecio@users.noreply.github.com>
2023-09-05 18:30:22 -0300

    Add files via upload

---
+++ Algoritmo356T/src/main/java/com/mycompany/algoritmo356t/Valores2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.algoritmo356t;
+
+/**
+ *
+ * @author diogo
+ */
+public class Valores2 {
+    int media[]= new int [15];
+    String nomes[]= new String[15];
+    String sit[]= new String[15]; 
+    float pr1[]= new float[15];
+    float pr2[]= new float[15]; 
+    
+}
