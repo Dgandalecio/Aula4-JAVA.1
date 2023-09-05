adc8aa8b51f9b598797ff3d943b3e9c364aea851
Dgandalecio <127789911+Dgandalecio@users.noreply.github.com>
2023-09-05 19:25:37 -0300

    Add files via upload

---
+++ Algoritmo360/src/main/java/com/mycompany/algoritmo360/Algoritmo360.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.algoritmo360;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author diogo
+ */
+public class Algoritmo360 {
+
+    public static void main(String[] args) {
+      int data, i, dia, mes;
+      
+        Valores4 sig = new Valores4();
+        
+        for( i=0; i<12; i++){
+            
+            String nome= JOptionPane.showInputDialog("Digite o Signo ");
+            sig.signo[i]= nome;
+            System.out.println(sig.signo[i]);
+            
+            String day = JOptionPane.showInputDialog("Digiteo ultimo dia ");
+            sig.ultdia[i]=Integer.parseInt(day);
+        }
+        data = Integer.parseInt( JOptionPane.showInputDialog("Digite a data no formato ddmm ou 9999 para terminar; "));
+        System.out.println(data);
+        
+        while(data!= 9999){
+            dia = data / 100;
+            mes = data % 100;
+            mes--;
+            
+            if (dia> sig.ultdia[mes]){
+                 mes = (mes + 1) % 12;   
+            }
+             System.out.println("Signo: " + sig.signo[mes]);
+
+            System.out.print("Digite a data no formato ddmm ou 9999 para terminar: ");
+            System.out.println(data);
+        }
+    }
+    }
+    
+
